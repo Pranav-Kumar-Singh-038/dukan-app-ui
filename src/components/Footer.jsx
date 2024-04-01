025c0c8524a8085374da11d37bd9d0ccb4257944
Pranav <119743240+Pranav-Kumar-Singh-038@users.noreply.github.com>
2024-04-01 20:04:02 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ export function Footer() {
         <div className="text-gray-400 text-sm flex flex-col h-24 justify-center items-center">
             <div className="flex">
                 <div>----- Made By</div >
-                <a href="google.com"><div className="text-blue-600">: Pranav Kumar Singh</div></a>
+                <a href="https://www.linkedin.com/in/pranav-kumar-singh-1a365124a/"><div className="text-blue-600">: Pranav Kumar Singh</div></a>
                 <div>, inspired by</div>
-                <a href="google.com">
+                <a href="https://dukan-app-ui.netlify.app/">
                     <div className="text-blue-600">- Dukaan App UI</div>
                 </a>
                 <div>-----</div>
             </div>
-            <a href="google.com" className="underline">
+            <a href="https://github.com/Pranav-Kumar-Singh-038/dukan-app-ui" className="underline">
                 <div>Get the source code</div>
             </a>
         </div >
